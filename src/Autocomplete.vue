f7553a748041b321e7d7081c0fe029c816cc6728
Peter Guskov <redmandarin@gmail.com>
2018-05-11 12:14:21 +0300

    Try to fix wrong scroll

--- src/Autocomplete.vue
+++ src/Autocomplete.vue
@@ -1,12 +1,12 @@
 <template lang="html">
   <div class="v-autocomplete">
     <div class="v-autocomplete-input-group" :class="{'v-autocomplete-selected': value}">
-      <input type="search" v-model="searchText" v-bind="inputAttrs" 
+      <input type="search" v-model="searchText" v-bind="inputAttrs"
             :class="inputAttrs.class || inputClass"
             :placeholder="inputAttrs.placeholder || placeholder"
             :disabled="inputAttrs.disabled || disabled"
             @blur="blur" @focus="focus" @input="inputChange"
-            @keyup.enter="keyEnter" @keydown.tab="keyEnter" 
+            @keyup.enter="keyEnter" @keydown.tab="keyEnter"
             @keydown.up="keyUp" @keydown.down="keyDown">
     </div>
     <div class="v-autocomplete-list" v-if="show">
@@ -19,8 +19,8 @@
 </template>
 
 <script>
-import Item from './Item.vue'
-import utils from './utils.js'
+import Item from './Item.vue';
+import utils from './utils.js';
 
 export default {
   name: 'v-autocomplete',
@@ -36,137 +36,145 @@ export default {
     items: Array,
     autoSelectOneItem: { type: Boolean, default: true },
     placeholder: String,
-    inputClass: {type: String, default: 'v-autocomplete-input'},
-    disabled: {type: Boolean, default: false},
-    inputAttrs: {type: Object, default: () => {return {}}},
-    keepOpen: {type: Boolean, default: false}
+    inputClass: { type: String, default: 'v-autocomplete-input' },
+    disabled: { type: Boolean, default: false },
+    inputAttrs: {
+      type: Object,
+      default: () => {
+        return {};
+      }
+    },
+    keepOpen: { type: Boolean, default: false }
   },
-  data () {
+  data() {
     return {
       searchText: '',
       showList: false,
       cursor: -1,
       internalItems: this.items || []
-    }
+    };
   },
   computed: {
-    hasItems () {
-      return !!this.internalItems.length
+    hasItems() {
+      return !!this.internalItems.length;
     },
-    show () {
-      return (this.showList && this.hasItems) || this.keepOpen
+    show() {
+      return (this.showList && this.hasItems) || this.keepOpen;
     }
   },
   methods: {
-    inputChange () {
-      this.showList = true
-      this.cursor = -1
-      this.onSelectItem(null, 'inputChange')
-      utils.callUpdateItems(this.searchText, this.updateItems)
-      this.$emit('change', this.searchText)
+    inputChange() {
+      this.showList = true;
+      this.cursor = -1;
+      this.onSelectItem(null, 'inputChange');
+      utils.callUpdateItems(this.searchText, this.updateItems);
+      this.$emit('change', this.searchText);
     },
 
-    updateItems () {
-      this.$emit('update-items', this.searchText)
+    updateItems() {
+      this.$emit('update-items', this.searchText);
     },
 
-    focus () {
-      this.$emit('focus', this.searchText)
-      this.showList = true
+    focus() {
+      this.$emit('focus', this.searchText);
+      this.showList = true;
     },
 
-    blur () {
-      this.$emit('blur', this.searchText)
-      setTimeout( () => this.showList = false, 200)
+    blur() {
+      this.$emit('blur', this.searchText);
+      setTimeout(() => (this.showList = false), 200);
     },
 
     onClickItem(item) {
-      this.onSelectItem(item)
-      this.$emit('item-clicked', item)
+      this.onSelectItem(item);
+      this.$emit('item-clicked', item);
     },
 
-    onSelectItem (item) {
+    onSelectItem(item) {
       if (item) {
-        this.internalItems = [item]
-        this.searchText = this.getLabel(item)
-        this.$emit('item-selected', item)
+        this.internalItems = [item];
+        this.searchText = this.getLabel(item);
+        this.$emit('item-selected', item);
       } else {
-        this.setItems(this.items)
+        this.setItems(this.items);
       }
-      this.$emit('input', item)
+      this.$emit('input', item);
     },
 
-    setItems (items) {
-      this.internalItems = items || []
+    setItems(items) {
+      this.internalItems = items || [];
     },
 
-    isSelectedValue (value) {
-      return 1 == this.internalItems.length && value == this.internalItems[0]
+    isSelectedValue(value) {
+      return 1 == this.internalItems.length && value == this.internalItems[0];
     },
 
-    keyUp (e) {
+    keyUp(e) {
       if (this.cursor > -1) {
-        this.cursor--
-        this.itemView(this.$el.getElementsByClassName('v-autocomplete-list-item')[this.cursor])
+        this.cursor--;
       }
     },
 
-    keyDown (e) {
+    keyDown(e) {
       if (this.cursor < this.internalItems.length) {
-        this.cursor++
-        this.itemView(this.$el.getElementsByClassName('v-autocomplete-list-item')[this.cursor])
+        this.cursor++;
       }
     },
 
-    itemView (item) {
+    itemView(item) {
       if (item && item.scrollIntoView) {
-        item.scrollIntoView(false)
+        item.scrollIntoView(false);
       }
     },
 
-    keyEnter (e) {
+    keyEnter(e) {
       if (this.showList && this.internalItems[this.cursor]) {
-        this.onSelectItem(this.internalItems[this.cursor])
-        this.showList = false
+        this.onSelectItem(this.internalItems[this.cursor]);
+        this.showList = false;
       }
-    },
-
+    }
   },
-  created () {
-    utils.minLen = this.minLen
-    utils.wait = this.wait
-    this.onSelectItem(this.value)
+  created() {
+    utils.minLen = this.minLen;
+    utils.wait = this.wait;
+    this.onSelectItem(this.value);
   },
   watch: {
-    items (newValue) {
-      this.setItems(newValue)
-      let item = utils.findItem(this.items, this.searchText, this.autoSelectOneItem)
+    items(newValue) {
+      this.setItems(newValue);
+      let item = utils.findItem(
+        this.items,
+        this.searchText,
+        this.autoSelectOneItem
+      );
       if (item) {
-        this.onSelectItem(item)
-        this.showList = false
+        this.onSelectItem(item);
+        this.showList = false;
       }
     },
-    value (newValue) {
-      if (!this.isSelectedValue(newValue) ) {
-        this.onSelectItem(newValue)
-        this.searchText = this.getLabel(newValue)
+    value(newValue) {
+      if (!this.isSelectedValue(newValue)) {
+        this.onSelectItem(newValue);
+        this.searchText = this.getLabel(newValue);
       }
     }
   }
-}
+};
 </script>
 
 <style>
-  .v-autocomplete {
-    position: relative;
-  }
-  .v-autocomplete .v-autocomplete-list {
-    position: absolute;
-  }
-  .v-autocomplete .v-autocomplete-list .v-autocomplete-list-item {
-    cursor: pointer;
-  }
-  .v-autocomplete .v-autocomplete-list .v-autocomplete-list-item.v-autocomplete-item-active {
-    background-color: #f3f6fa;
-  }
+.v-autocomplete {
+  position: relative;
+}
+.v-autocomplete .v-autocomplete-list {
+  position: absolute;
+}
+.v-autocomplete .v-autocomplete-list .v-autocomplete-list-item {
+  cursor: pointer;
+}
+.v-autocomplete
+  .v-autocomplete-list
+  .v-autocomplete-list-item.v-autocomplete-item-active {
+  background-color: #f3f6fa;
+}
 </style>
